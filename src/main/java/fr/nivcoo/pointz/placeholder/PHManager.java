1d4274b2b84dbc19d5f67a7746a1088b3b3ec478
nivcoo <nico64800@live.fr>
2020-03-03 11:18:09 +0100

    improv. error with mvdw placeholder

--- src/main/java/fr/nivcoo/pointz/placeholder/PHManager.java
+++ src/main/java/fr/nivcoo/pointz/placeholder/PHManager.java
@@ -1,6 +1,5 @@
 package fr.nivcoo.pointz.placeholder;
 
-import be.maximvdw.placeholderapi.PlaceholderAPI;
 import fr.nivcoo.pointz.Pointz;
 
 public class PHManager {
@@ -9,7 +8,7 @@ public class PHManager {
 
 	public static void registerMVDW(String path) {
 
-		PlaceholderAPI.registerPlaceholder(pointz, path, new fr.nivcoo.pointz.placeholder.placeholder.MVDWPlaceHolderAPI(pointz));
+		be.maximvdw.placeholderapi.PlaceholderAPI.registerPlaceholder(pointz, path, new fr.nivcoo.pointz.placeholder.placeholder.MVDWPlaceHolderAPI(pointz));
 
 	}
 
